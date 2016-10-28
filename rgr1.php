e4899d6b65f6092526106405d3d2d824c089f08d
Dilyaferuz <dilyaferuz777@gmail.com>
2016-10-28 14:09:31 +0700

    Add files via upload

--- rgr1.php
+++ rgr1.php
@@ -1,65 +1,65 @@
 <html>
-<head> <meta charset="UTF-8">
-
+<head> 
+<meta charset="UTF-8">
 </head>
 <body>
 <?php
-	$flats = [
-		'Однокомнатные' => [
-			[
-				'address' => 'Вилюйская, 5',
-				'cost' => 2190000,
-				'areaTotal' => 47,
-				'areaLive' => 15,
-				'areaKitchen' => 11,
-				'photos' => [
-					'http://cdn.n1.ru/cache/realty/photo/52dc0385b421b5f67cb8f7a970bd0d7a_1200_800.jpg',
-					'http://cdn.n1.ru/cache/realty/photo/8b54efe6c16e78d8aca5b21bc4250e6f_1200_800.jpg'
-				]
-			],
-			[
-				'address' => 'Дуси Ковальчук, 238',
-				'cost' => 2750000,
-				'areaTotal' => 40,
-				'areaLive' => 15,
-				'areaKitchen' => 12,
-				'photos' => [
-					'http://cdn.n1.ru/cache/realty/photo/48efa8f6e915699aa161fc02742b9f1f_1200_800.jpg',
-					'http://cdn.n1.ru/cache/realty/photo/7c50133db0add15b8d7ff50aa8138b42_1200_800.jpg'
-				]
-			],
-			[
-				'address' => 'Зорге, 229\3',
-				'cost' => 960000,
-				'areaTotal' => 31,
-				'areaLive' => 18,
-				'areaKitchen' => 0,
-				'photos' => [
-				]
+$flats = [
+	'Однокомнатные' => [
+		[
+			'address' => 'Вилюйская, 5',
+			'cost' => 2190000,
+			'areaTotal' => 47,
+			'areaLive' => 15,
+			'areaKitchen' => 11,
+			'photos' => [
+				'http://cdn.n1.ru/cache/realty/photo/52dc0385b421b5f67cb8f7a970bd0d7a_1200_800.jpg',
+				'http://cdn.n1.ru/cache/realty/photo/8b54efe6c16e78d8aca5b21bc4250e6f_1200_800.jpg'
 			]
 		],
-		'Двухкомнатные' => [
-			[
-				'address' => 'Забалуева, 84',
-				'cost' => 1385000,
-				'areaTotal' => 41,
-				'areaLive' => 23,
-				'areaKitchen' => 4,
-				'photos' => [
-				]
-			],
-			[
-				'address' => 'Петухова, 156',
-				'cost' => 2670000,
-				'areaTotal' => 56,
-				'areaLive' => 25,
-				'areaKitchen' => 10,
-				'photos' => [
-					'http://cdn.n1.ru/cache/realty/photo/a6b5fa7dc66fe26ba7fc34bb5a409ba5_1200_800.jpg'
-				]
+		[
+			'address' => 'Дуси Ковальчук, 238',
+			'cost' => 2750000,
+			'areaTotal' => 40,
+			'areaLive' => 15,
+			'areaKitchen' => 12,
+			'photos' => [
+				'http://cdn.n1.ru/cache/realty/photo/48efa8f6e915699aa161fc02742b9f1f_1200_800.jpg',
+				'http://cdn.n1.ru/cache/realty/photo/7c50133db0add15b8d7ff50aa8138b42_1200_800.jpg'
+			]
+		],
+		[
+			'address' => 'Зорге, 229\3',
+			'cost' => 960000,
+			'areaTotal' => 31,
+			'areaLive' => 18,
+			'areaKitchen' => 0,
+			'photos' => [
 			]
 		]
-	];
+	],
+	'Двухкомнатные' => [
+		[
+			'address' => 'Забалуева, 84',
+			'cost' => 1385000,
+			'areaTotal' => 41,
+			'areaLive' => 23,
+			'areaKitchen' => 4,
+			'photos' => [
+			]
+		],
+		[
+			'address' => 'Петухова, 156',
+			'cost' => 2670000,
+			'areaTotal' => 56,
+			'areaLive' => 25,
+			'areaKitchen' => 10,
+			'photos' => [
+				'http://cdn.n1.ru/cache/realty/photo/a6b5fa7dc66fe26ba7fc34bb5a409ba5_1200_800.jpg'
+			]
+		]
+	]
+];
 foreach($flats as $room => $type){  
 	echo " <p> <div class='area'> $room </div> <p> ";
 	foreach ($type as $room ) {?>
@@ -74,15 +74,15 @@ foreach($flats as $room => $type){
 					<?php 
 				}
 				if (isset ($room['areaTotal'])){
-					?> <div class="home"> Общая площадь: <?=htmlspecialchars($room['areaTotal'])?> </div>
+					?> <div class="home"> Общая площадь: <?=htmlspecialchars($room['areaTotal'])?> кв.м </div>
 					<?php 
 				}
 				if (isset ($room['areaLive'])){
-					?> <div class="home"> Жилая площадь: <?=htmlspecialchars($room['areaLive'])?> </div>
+					?> <div class="home"> Жилая площадь: <?=htmlspecialchars($room['areaLive'])?> кв.м </div>
 					<?php
 				}
 				if (isset ($room['areaKitchen'])){
-					?> <div class="home"> Кухонная площадь: <?=htmlspecialchars($room['areaKitchen'])?> </div>
+					?> <div class="home"> Кухонная площадь: <?=htmlspecialchars($room['areaKitchen'])?> кв.м </div>
 					<?php 
 				} ?>
 			</div>
@@ -90,7 +90,7 @@ foreach($flats as $room => $type){
 		<div class="photos"> <?php
 			if (isset($room['photos'])){
 				foreach ($room['photos'] as $key => $photo) {
-					echo "<p><img class='photo' src=$photo><p>";
+					echo "<p><img class='photo' src='".htmlspecialchars($photo)." '><p>";
 				}
 			} ?>
 		</div> 
@@ -126,7 +126,7 @@ foreach($flats as $room => $type){
  } 
  div.view{
 	float: right;
-	width: 600px;
+	width: 1000px;
  }
  .flat{
 	padding-left: 20px;
